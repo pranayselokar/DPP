9327929a690a7d9938d09c47e524546f4d09b5d9
pranayselokar <pranayselokar2@gmail.com>
2022-07-27 08:37:43 +0530

    leapyear file added

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LeapYear
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int y;
+            Console.WriteLine(" Please enter the year in four digits ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            if ((y % 400) == 0 && (y % 4) == 0) ;
+            {
+                Console.WriteLine(" It is a leap year " + y);
+            }
+     
+            else if ((y % 100) == 0) ;
+            {
+                Console.WriteLine(" It is not a leap year " + y);
+            }
+
+            else
+            {
+                Console.WriteLine(" It is not a leap year" + y);
+            }
+        }
+    }
+}
+            
+           
+
